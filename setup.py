f71dc6f99344d3f3f601f6ff6347f1082d2fa6c3
Arthur Lutz <arthur.lutz@logilab.fr>
2021-11-16 11:21:19 +0100

    [setup] pin down redis to avoid 4.0 breaking changes

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open(os.path.join(HERE, "README.md")) as f:
 
 # set up requires
 install_requires = [
-    "redis>=2.4.11, != 2.9.1",
+    "redis>=2.4.11, != 2.9.1, < 4.0.0",
     "pyramid>=1.3",
     "six",
     "zope.interface",  # in Pyramid
